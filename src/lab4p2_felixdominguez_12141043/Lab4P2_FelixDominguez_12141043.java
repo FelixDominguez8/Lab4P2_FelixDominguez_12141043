df4b9eeb442a0b48670ae18ec88509fd589512e7
Felix Dominguez <66543823+StickyFingers84@users.noreply.github.com>
2022-02-11 16:15:45 -0600

    Quinto Commit
    Defaults e impresion

--- src/lab4p2_felixdominguez_12141043/Lab4P2_FelixDominguez_12141043.java
+++ src/lab4p2_felixdominguez_12141043/Lab4P2_FelixDominguez_12141043.java
@@ -19,6 +19,15 @@ public class Lab4P2_FelixDominguez_12141043 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        familias.add(new Familia("Capuleto"));
+        familias.add(new Familia("Montesco"));
+        familias.add(new Familia("Vallecillos"));
+        ((Familia)familias.get(0)).getAldeanos().add(new Normal("Julieta", "Capuleto", 18, 100));
+        ((Familia)familias.get(1)).getAldeanos().add(new Supergranjero("Romeo", "Montesco", 18, 1000));
+        ((Familia)familias.get(1)).getAldeanos().add(new Herrero("Carlos", "Montesco", 32, 800));
+        ((Familia)familias.get(1)).getAldeanos().add(new Agronomo("Juan", "Montesco", 28, 600));
+        ((Familia)familias.get(0)).getAldeanos().add(new Agronomo("Julia", "Capuleto", 22, 600));
+        ((Familia)familias.get(0)).getAldeanos().add(new Herrero("Monica", "Capuleto", 28, 800));
         char resp='s';
         while(resp=='s' || resp=='S'){
             System.out.println("");
@@ -44,7 +53,6 @@ public class Lab4P2_FelixDominguez_12141043 {
                     System.out.println("3) Herrero");
                     System.out.println("4) Agronomo");
                     System.out.println("5) Explosivo");
-                    System.out.println("6) Supergranjero");
                     System.out.println("Que tipo de aldeano desea: ");
                     int tipo=lea.nextInt();
                     System.out.println("Ingrese el nombre: ");
@@ -57,11 +65,39 @@ public class Lab4P2_FelixDominguez_12141043 {
                     System.out.println("Ingrese la vida: ");
                     int vida=lea.nextInt();
                     for(int i=0;i<familias.size();i++){
-                        
+                        if(((Familia)familias.get(i)).getLinaje().equals(apellido)){
+                            switch(tipo){
+                                case 1:{
+                                    ((Familia)familias.get(i)).getAldeanos().add(new Normal(nombre, apellido, edad, vida));
+                                    break;
+                                }
+                                case 2:{
+                                    ((Familia)familias.get(i)).getAldeanos().add(new Pacifista(nombre, apellido, edad, vida));
+                                    break;
+                                }
+                                case 3:{
+                                    vida=((vida/100)*50)+vida;
+                                    ((Familia)familias.get(i)).getAldeanos().add(new Herrero(nombre, apellido, edad, vida));
+                                    break;
+                                }
+                                case 4:{
+                                    ((Familia)familias.get(i)).getAldeanos().add(new Agronomo(nombre, apellido, edad, vida));
+                                    break;
+                                }
+                                case 5:{
+                                    ((Familia)familias.get(i)).getAldeanos().add(new Explosivo(nombre, apellido, edad, vida));
+                                    break;
+                                }
+                            }
+                        }
                     }
                     break;
                 }
                 case 3:{
+                    String salida="";
+                    for(Object temp:familias){
+                        salida+=""+familias.indexOf(temp)+" - "+temp+"\n";
+                    }
                     break;
                 }
                 case 4:{
