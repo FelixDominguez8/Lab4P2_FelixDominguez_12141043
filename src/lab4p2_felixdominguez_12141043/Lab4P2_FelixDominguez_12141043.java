99e55d3e2e7cd8978694b77f1629bfcc0af48e1c
Felix Dominguez <66543823+StickyFingers84@users.noreply.github.com>
2022-02-11 13:52:01 -0600

    Primer commit
    Inicio

---
+++ src/lab4p2_felixdominguez_12141043/Lab4P2_FelixDominguez_12141043.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_felixdominguez_12141043;
+
+/**
+ *
+ * @author Lenovo I7
+ */
+public class Lab4P2_FelixDominguez_12141043 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola");
+    }
+    
+}
