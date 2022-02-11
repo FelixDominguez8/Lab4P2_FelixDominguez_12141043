b56f3abe05004c5384be78951af9689e44bc7e7f
Felix Dominguez <66543823+StickyFingers84@users.noreply.github.com>
2022-02-11 14:07:50 -0600

    Segundo Commit
    Clases

---
+++ src/lab4p2_felixdominguez_12141043/Familia.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_felixdominguez_12141043;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo I7
+ */
+public class Familia {
+    private String linaje;
+    private ArrayList<Aldeano> aldeanos=new ArrayList();
+}
---
+++ src/lab4p2_felixdominguez_12141043/Pacifista.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_felixdominguez_12141043;
+
+/**
+ *
+ * @author Lenovo I7
+ */
+public class Pacifista {
+    
+}
